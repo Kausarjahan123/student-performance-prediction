1ec6e54bc492b6c1713334cc09bdcafff694016d
KAUSAR JAHAN <jkausar2024@gmail.com>
2026-06-10 12:09:38 -0500

    Add student performance prediction using Streamlit

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import pickle
+import pandas as pd
 
+model = pickle.load(open("model.pkl", "rb"))
+
+st.title("📊 Student Performance Predictor")
+
+studytime = st.number_input("Study Time", 1, 4)
+failures = st.number_input("Failures", 0, 5)
+absences = st.number_input("Absences", 0, 50)
+
+input_df = pd.DataFrame([{
+    "studytime": studytime,
+    "failures": failures,
+    "absences": absences
+}])
+
+if st.button("Predict"):
+    pred = model.predict(input_df)[0]
+    st.success(f"Predicted Grade: {pred:.2f}")
